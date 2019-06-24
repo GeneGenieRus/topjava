f8051c8b8f80fe3e65e2e09baeaaab19a4765222
NewUser <newuser@example.com>
2019-06-24 16:48:21 +0300

    add sql scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, datetime, calories, user_id) VALUES
+('User завтрак', '2019-06-24 10:00:00' , 600, 100000),
+('User обед', '2019-06-24 15:00:00' , 1300, 100000),
+('User ужин', '2019-06-24 18:00:00' , 200, 100000),
+('Admin завтрак', '2019-06-24 10:00:00' , 500, 100001),
+('Admin обед', '2019-06-24 15:00:00', 1500, 100001),
+('Admin ужин', '2019-06-24 18:00:00' , 1500, 100001);
